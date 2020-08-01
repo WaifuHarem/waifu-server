755ce485a43419afe0ada8af5eeb7e0aca332d41
Layla A <lights@reachinghalcyon.com>
2020-08-01 01:07:31 -0400

    Replace .hasOwnProperty with [] syntax

--- modules/methods.js
+++ modules/methods.js
@@ -12,86 +12,86 @@ const Crypto = require("crypto");
 const GameNames = [null, "Etterna", "osu!mania", "FFR", "Quaver", "Robeats"];
 
 class Methods {
-    constructor() {} // Static
+	constructor() {} // Static
 
-    // v0.1.0
-    static VerifyData(data) {
-        return data && data.userid;
-    }
+	// v0.1.0
+	static VerifyData(data) {
+		return data && data.userid;
+	}
 
-    // v0.1.0
-    static async VerifyPermission(userid, opcode, override) {
-        let gate = override ? override : config.keys[opcode];
-        if (gate === 0)
-            return true; // No auth required.
-        if (!await Methods.VerifyRegistration(userid))
-            return false; // Opcodes 1 and 2 require registration
-        switch(gate) {
-            case 1: return true;
-            case 2: return await Methods.VerifyAdmin(userid);
-            default: return false; // Shouldn't be reachable
-        }
-    }
+	// v0.1.0
+	static async VerifyPermission(userid, opcode, override) {
+		let gate = override ? override : config.keys[opcode];
+		if (gate === 0)
+			return true; // No auth required.
+		if (!await Methods.VerifyRegistration(userid))
+			return false; // Opcodes 1 and 2 require registration
+		switch(gate) {
+		case 1: return true;
+		case 2: return await Methods.VerifyAdmin(userid);
+		default: return false; // Shouldn't be reachable
+		}
+	}
 
-    // v0.1.0
-    static VerifyScoreIntegrity(scoreData) {
-        if (!scoreData.game || typeof scoreData.game !== "number" || scoreData.game >= 5)
-            return false;
+	// v0.1.0
+	static VerifyScoreIntegrity(scoreData) {
+		if (!scoreData.game || typeof scoreData.game !== "number" || scoreData.game >= 5)
+			return false;
 
-        let errors = 0;
-        for (const [key, type] of Data.GetScoreSchema(scoreData.game)) {
-            if (!scoreData.hasOwnProperty(key) || typeof scoreData[key] !== type)
-                errors++;
-        }
+		let errors = 0;
+		for (const [key, type] of Data.GetScoreSchema(scoreData.game)) {
+			if (!scoreData[key] || typeof scoreData[key] !== type)
+				errors++;
+		}
 
-        return errors === 0;
-    }
+		return errors === 0;
+	}
 
-    // v0.1.0
-    static async VerifyRegistration(userid) {
-        let reply = await Database.query("SELECT * FROM Users WHERE userid = ?", [userid]);
-        return reply && reply[0];
-    }
+	// v0.1.0
+	static async VerifyRegistration(userid) {
+		let reply = await Database.query("SELECT * FROM Users WHERE userid = ?", [userid]);
+		return reply && reply[0];
+	}
 
-    // v0.1.0
-    static async VerifyAdmin(userid) {
-        let reply = await Database.query("SELECT Group FROM Users WHERE userid = ?", [userid]);
-        return reply && reply[0] && reply[0].group === 2;
-    }
+	// v0.1.0
+	static async VerifyAdmin(userid) {
+		let reply = await Database.query("SELECT Group FROM Users WHERE userid = ?", [userid]);
+		return reply && reply[0] && reply[0].group === 2;
+	}
 
-    // v0.1.0
-    static async VerifyScoreOwnership(userid, scoreData) {
-        let column = (["", "NameEtterna", "NameOsu", "NameFFR", "NameQuaver", "NameRobeats"])[scoreData.game];
-        let reply = await Database.query("SELECT ? FROM Users WHERE userid = ?", [column, userid]);
-        return reply && reply[0] && reply[0][column] && reply[0][column] === scoreData.player;
-    }
+	// v0.1.0
+	static async VerifyScoreOwnership(userid, scoreData) {
+		let column = (["", "NameEtterna", "NameOsu", "NameFFR", "NameQuaver", "NameRobeats"])[scoreData.game];
+		let reply = await Database.query("SELECT ? FROM Users WHERE userid = ?", [column, userid]);
+		return reply && reply[0] && reply[0][column] && reply[0][column] === scoreData.player;
+	}
 
-    // v0.1.0
-    static HashScore(scoreData) {
-        const game = GameNames[scoreData.game];
-        const title = Methods.toId(scoreData.title);
-        const artist = Methods.toId(scoreData.artist);
-        const creator = Methods.toId(game === "Etterna" ? scoreData.packname : scoreData.creator);
-        const diff = Methods.toId(game === "FFR" ? "" : scoreData.diff);
-        const scoreString = `${game}-${title}-${artist}[${creator}-${diff}]`;
-        return Methods.HashScoreString(scoreString);
-    }
+	// v0.1.0
+	static HashScore(scoreData) {
+		const game = GameNames[scoreData.game];
+		const title = Methods.toId(scoreData.title);
+		const artist = Methods.toId(scoreData.artist);
+		const creator = Methods.toId(game === "Etterna" ? scoreData.packname : scoreData.creator);
+		const diff = Methods.toId(game === "FFR" ? "" : scoreData.diff);
+		const scoreString = `${game}-${title}-${artist}[${creator}-${diff}]`;
+		return Methods.HashScoreString(scoreString);
+	}
 
-    // v0.1.0
-    static HashScoreString(scoreString) {
-        return Crypto.createHash("sha256").update(scoreString).digest("base64");
-    }
+	// v0.1.0
+	static HashScoreString(scoreString) {
+		return Crypto.createHash("sha256").update(scoreString).digest("base64");
+	}
 
-    static async AddScore(query) {
-        let error = await Database.query(...query);
-        console.log(error); // Temporary log - remove me after testing is complete
-        return error;
-    }
+	static async AddScore(query) {
+		let error = await Database.query(...query);
+		console.log(error); // Temporary log - remove me after testing is complete
+		return error;
+	}
 
-    // v0.1.0
-    static toId(string) {
-        return String(string).toLowerCase().replace(/[^a-z0-9]+/g, '');
-    }
+	// v0.1.0
+	static toId(string) {
+		return String(string).toLowerCase().replace(/[^a-z0-9]+/g, "");
+	}
 }
 
 module.exports = Methods;
--- modules/parser.js
+++ modules/parser.js
@@ -35,16 +35,16 @@ class Operations {
 				let query = [];
 				let hash = Methods.HashScore(scoreData);
 				switch(scoreData.game) { // Rejections are temporary until implementation
-					default:
-					case 0: return reject(4);
-					case 1: return reject(0); // break;
-					case 2: return reject(0); // break;
-					case 3:
-						query.push("INSERT INTO FFR_Scores(ScoreHash, UserID, Quarantine, Player, Title, Artist, Creator, W0, W1, W2, W3, W4, W5, Equiv, Raw, TimeAchieved) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
-						query.push([hash, userid, scoreData.quarantine, scoreData.player, scoreData.title, scoreData.artist, scoreData.creator, scoreData.W0, scoreData.W1, scoreData.W2, scoreData.W3, scoreData.W4, scoreData.W5, scoreData.equiv, scoreData.raw, scoreData.date]);
-						break;
-					case 4: return reject(0); // break;
-					case 5: return reject(0); // break;
+				default:
+				case 0: return reject(4);
+				case 1: return reject(0); // break;
+				case 2: return reject(0); // break;
+				case 3:
+					query.push("INSERT INTO FFR_Scores(ScoreHash, UserID, Quarantine, Player, Title, Artist, Creator, W0, W1, W2, W3, W4, W5, Equiv, Raw, TimeAchieved) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+					query.push([hash, userid, scoreData.quarantine, scoreData.player, scoreData.title, scoreData.artist, scoreData.creator, scoreData.W0, scoreData.W1, scoreData.W2, scoreData.W3, scoreData.W4, scoreData.W5, scoreData.equiv, scoreData.raw, scoreData.date]);
+					break;
+				case 4: return reject(0); // break;
+				case 5: return reject(0); // break;
 				}
 				let err = await Methods.AddScore(query);
 				if (err) {
@@ -102,17 +102,17 @@ function receive(data) {
 
 			let reply = null;
 			switch(data.opcode) {
-				case 1: reply = await Operations.AddScore(data.userid, data.data || null); break;
-				case 2: reply = await Operations.RemoveScore(); break;
-				case 3: reply = await Operations.Fetch(); break;
-				case 4: reply = await Operations.Session(); break;
-				case 5: reply = await Operations.Multiplayer(); break;
-				case 6: reply = await Operations.Register(); break;
-				case 7: reply = await Operations.Deregister(); break;
-				default: reply = {code: 0};
+			case 1: reply = await Operations.AddScore(data.userid, data.data || null); break;
+			case 2: reply = await Operations.RemoveScore(); break;
+			case 3: reply = await Operations.Fetch(); break;
+			case 4: reply = await Operations.Session(); break;
+			case 5: reply = await Operations.Multiplayer(); break;
+			case 6: reply = await Operations.Register(); break;
+			case 7: reply = await Operations.Deregister(); break;
+			default: reply = {code: 0};
 			}
 			return resolve(reply);
-		}
+		};
 		wrapper(resolve, reject, data);
 	}).catch(err => {
 		Crash(err, "Received data that cannot be processed.");
