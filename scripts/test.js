c495090b4416e467fa1e3e5374dfea368e0dac83
Layla A <lights@reachinghalcyon.com>
2020-08-04 13:36:37 -0400

    Test commit file

---
+++ scripts/test.js
@@ -0,0 +1,13 @@
+"use strict";
+
+console.log("This file a commit test file for linting and automation purposes only. Not for inclusion in the server.");
+
+// Properly linted
+
+let test = "true";
+
+// Improperly linted
+
+function testfun() {
+        test = 'true'
+}
